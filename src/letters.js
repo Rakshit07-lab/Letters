51a58cd358a7788f265ce761dcbfc4b635092fde
Rakshit Raj <rakshitraj@kgpian.iitkgp.ac.in>
2025-12-06 23:08:55 +0530

    updated letters

--- src/letters.js
+++ src/letters.js
@@ -1,51 +1,50 @@
 export const letters = [
             {
                 day: 1,
-                title: "Day 1: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 1: Me meeting you for the first time",
+                content: `I remember meeting you at the bridge for the first time, it was winters maybe, I was with Aniruddha, you were with Manasvi. Sneh was also there. We had chai, and something to eat at cheddis. Now the question was who will pay. we played a game for that and finally all thanks to my luck that i eneded up paying for all of you. I also remember you becoming an aeroplane to show that you are from aero dept. Crazy first meeting. Already felt the Jahnavi energy.`,
                 color: "gradient-1"
             },
             {
                 day: 2,
-                title: "Day 2: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 2: The picnic day",
+                content: `You organised a picnic, something which i could have never imagined of, again "Jahnavi Energy". I was also amazed that you had put my photo in that identify the person wala game. I thought I was just another person passing by you during your college life, thanks to the geedian group. Also that was the day you saw me spreading nutella over the bread or whatever it was. But that was so husband material. Imagine making an account for me on shadi.com and losing me to some random girl on the site. HAHAHA`,
                 color: "gradient-2"
             },
             {
                 day: 3,
-                title: "Day 3: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-`,
+                title: "Day 3: Teamkart",
+                content: `You called me asking something about the task and I think I was brushing, hahaha. I dont know what was that fine line, when we actually became good friends. I think it was the december, or maybe i dont know. But one thing is for sure that it was teamkart and rant about people and car at TeamKart, that brought us closer. Teamkart was the pivotal point of our love journey. Also maybe it was my dad jokes, or me casually making me the subject of roast. You tell me what was it or what was that timeline that you fell in love with me.`,
                 color: "gradient-3"
             },
             {
                 day: 4,
-                title: "Day 4: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 4: You me and bay top",
+                content: `The day you forcibly convinced me to take you to park and get drunk. Everything that happened that day was so special. The fun things we did, ran over the highway, walked on railway track- all the way to hijli, sitting and talking on bay top, you lying by me in the garden infront of nehru meuseum. The way you held my hand. everytime you kept your hand over my chest, my heart started pumping at 2x speed. You putting your head over my chest and counting my heartbeats. I was already having some feeling for you by then, but was not sure what that meant in reality. I just chose to ignore all of them, cause i didnt want to lose that friendship atleast. We even video called each other after reaching our rooms, just to make sure that we are fine. That was too cuteeeeeee`,
                 color: "gradient-4"
             },
             {
                 day: 5,
-                title: "Day 5: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 5: Kolkata reunion",
+                content: `one more day well spent with Jahnavi. Every meter of that way back to campus was so beautiful, only beacuse you were with me. The moment you slept on my lap, oh my god, my heart again started pumping at 2x speed, then you started holding my fingers, I thought you are already asleep and you are doing it unintentionally, but i could just not remove your fingers from my hand. and then when i removed my hand from there, you took my hand and put that over your thighs and i was already half dead. God what is happening, am I dreaming. The movie, the food, the chit chats we had, everything was so amazing.`,
                 color: "gradient-5"
             },
             {
                 day: 6,
-                title: "Day 6: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 6: The Reveal day",
+                content: `we were back on campus, I already started feeling some tension between us, then you finally called me to meet. We talked, we met a lot of people, we started our project stapler car and failed drastically, and them we sat by the lake. You again held my hand with your fingers, i started getting butterflies again. My heart pumping at 2x again, the magical feeling came back again. OHMYGOD I cant unfeel those moments. Finally you proposed and I was like wtf, is this real, ohmygod. AND rest everything thereafter was beautiful`,
                 color: "gradient-6"
             },
             {
                 day: 7,
-                title: "Day 7: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                title: "Day 7: The Puri Trip",
+                content: `We decided on the puri trip finally and thankfully it wasn't a solo trip for you. this was the benchmark trip for our relationship. I just hope we make more and more trips like this. Maybe national or internatiaonal, nut most important part of the trip would be you and me. I loveeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuh Jahnavi, my cutuuuuu, putttuuuu, rasmalaaaai, kaju katli, boondi ka laddoooo. I just cant wait to have tabadtod chumma chaati session with you once again`,
                 color: "gradient-7"
             },
             {
                 day: 8,
-                title: "Day 8: Title Goes Here",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+                title: "Day 8: I miss you, but i love you more",
+                content: `Days are passing and our bonds getting more and more strong. I know its an undeniable fact that we will have to part away after few months for a very long period of time, but  just dont want to think about it and make it ruin our present happiness. I just want to enjoy the present, every moment that I have with you right now. We will figure out somehow how to manage the long distance thing, but for now lets focus on our short distance thing and lets shorten the distance between us more and more, Just come close to me....more close...closerr....more closerrrrr.......just dont o anywhere.`,
                 color: "gradient-7"
             }
         ];
@@ -53,12 +52,12 @@ export const letters = [
 
 
 export const wordPuzzles = {
-            1: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            2: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            3: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            4: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            5: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            6: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            7: { word: "GUESS", hint: "Hint that only the two of us know!" },
-            8: { word: "GUESS", hint: "Hint that only the two of us know!" }
+            1: { word: "MADDY", hint: "my first love from INSTA" },
+            2: { word: "TANNU", hint: "who do you sleep with in your room (5 letters)" },
+            3: { word: "ARYAN", hint: "I slept with this person for 2 months" },
+            4: { word: "ANMOL", hint: "Most disturbing guy during our intense sessions" },
+            5: { word: "NASKR", hint: "BEST prof award goes to (5 letter)" },
+            6: { word: "MAZAA", hint: "Name of our child" },
+            7: { word: "CARDS", hint: "What did we play in puri at night" },
+            8: { word: "DUREX", hint: "Which company cards did we use in puri" }
         };
